b6787973cc029a13aaa26c09e6c1b7b6b141fd2a
Georgiy Demo <demenchuk.george@protonmail.com>
2020-05-18 14:14:18 +0300

    Частично вынесли логику получения расписания

--- ruztimetable/main.py
+++ ruztimetable/main.py
@@ -21,19 +21,35 @@ from flask_restful import Resource, Api
 import json
 from fa_api import FaAPI
 from mongo_module import MongoUserClass
-from user_module import UserCommandsClass, get_timetable
+from user_module import UserCommandsClass
+
 app = Flask(__name__)
 api = Api(app)
 mongo = MongoUserClass()
 
 
-#Штука для временного хранения данных о пользователе
 buf_userdict = {}
-
 class MainClass(Resource):
 
+    #TODO
+    def json_generator(self, text, buttons=None):
+        """Генерация ответа в json"""
+        
+        out_dict = {
+            "response":
+                {"end_session": False, 
+                },
+            "version": "1.0",    
+        }
+
+        out_dict['response']['text'] = text
+        if buttons is not None:
+            out_dict['response']['buttons'] = [{"title": txt,"hide": True} for txt in buttons]
+
+        return out_dict
+
     def search_group(self, group_name):
-        """Метод для поиска группы"""
+        """Поиск группы"""
 
         group_name = group_name.replace(" ","")
         
@@ -53,13 +69,14 @@ class MainClass(Resource):
 
     def post(self):
 
-        out_dict = {"response": {"end_session": False},"version": "1.0"}
+        out_dict = {}
+
         req = request.json
         user_id = req['session']['user_id']
 
         #Если новый пользователь и его нет в таблице, значит это самое начало
         if req['session']['new'] and mongo.find_user(user_id):
-            out_dict['response']['text'] = 'Привет, для начала скажи название своей группы'
+            out_dict = self.json_generator("Привет, для начала скажи название своей группы")
         
         #Если новый пользователь сказал название группы
         elif mongo.find_user(user_id):
@@ -74,14 +91,14 @@ class MainClass(Resource):
                 user_data = buf_userdict[user_id]
 
                 mongo.set_usergroup(user_id, user_data["group_id"], user_data["group_name"])
-                out_dict['response']['text'] = "Хорошо, я выставила группу {} для вас. Скажите \"расписание на сегодня\" для получения расписания.\nТакже могу подсказать расписание на завтра и любую другую дату".format(user_data["group_name"])
-
+                message_str = "Хорошо, я выставила группу {} для вас. Скажите \"расписание на сегодня\" для получения расписания.\nТакже могу подсказать расписание на завтра и любую другую дату".format(user_data["group_name"])
+                out_dict = self.json_generator(message_str,["Расписание на сегодня","Расписание на завтра","Изменение группы"])
                 del buf_userdict[user_id]
 
             #Если пользователь не согласился со своей группой
 
             elif user_command in disagreement_words and user_id in buf_userdict:
-                out_dict['response']['text'] = "Хорошо, попробуйте произнести название группы еще раз"
+                out_dict = self.json_generator("Хорошо, попробуйте произнести название группы еще раз")
                 del buf_userdict[user_id]
 
             #Пользователь всёж сказал именно название группы
@@ -90,26 +107,19 @@ class MainClass(Resource):
                 #Ищем группу
                 search_flag, search_dict = self.search_group(user_command)
                 if not search_flag:
-                    out_dict['response']['text'] = 'Я не смогла найти вашу группу, извините.\nМожет попробуете назвать ее заново?'
+                    out_dict = self.json_generator("Я не смогла найти вашу группу, извините.\nМожет попробуете назвать ее заново?")
 
                 else:
-                    out_dict['response']['text'] = "Ваша группа относится к "+search_dict["description"]+", правильно ?"
+                    out_dict = self.json_generator("Ваша группа относится к "+search_dict["description"]+", правильно ?",["Да", "Нет"])
                     buf_userdict[user_id] = search_dict
 
 
         else:
-           
-            #Если действующий пользователь, то даем ему одно из расписаний
             
-            """
-                - Расписание на сегодня/расписание - показывает расписание на сегодня
-                - Расписание на завтра
-                - Расписание на послезавтра
-                - Расписание на ДАТА 01.03.2020 ?
-                - Возможность сменить группу
-            """
-            result_str = get_timetable(user_id, mongo)
-            out_dict['response']['text'] = result_str
+            #Если действующий пользователь, то даем ему одно из расписаний
+            user_command = req["request"]["command"]
+            obj = UserCommandsClass(user_id, user_command)
+            out_dict = self.json_generator(obj.out_str, obj.out_buttons)
         
         return out_dict
            
--- ruztimetable/user_module.py
+++ ruztimetable/user_module.py
@@ -1,26 +1,43 @@
 from fa_api import FaAPI
-import pymongo
+from mongo_module import MongoUserClass
 
-def get_timetable(user_id, connector, date=None):
-
-    fa = FaAPI()
-    mongo_result = connector.get_usergroup(user_id)
-    group_name, group_id = mongo_result["group_name"], mongo_result["group_id"]
-    #Получаем инфо о расписании группы 
-    timetable = fa.timetable_group(group_id, date, date)
-
-    if len(timetable) == 0:
-        return "Сегодня пар нет, отдыхайте!"
-
-    out_str = "Расписание группы {} на {}:\n".format(group_name, "date")
-    for i in range(len(timetable)):
-        out_str +="{}. {}\n".format(i+1,timetable[i]["discipline"])
-    return out_str
 
 class UserCommandsClass():
     """Класс обработки команд от пользователя"""
     def __init__(self, user_id):
         self.out_str = None
+        self.out_buttons = None
         self.user_id = user_id
-    
+        self.connector = MongoUserClass()
+
+        router_dict = {
+            "расписание" : 
+            "Расписание на сегодня"
+            "расписание на завтра"
+            "расписание на послезавтра"
+            "Изменение группы",
+            "Расписаине на ДАТА"
+            "": Просто 
+
+        }
+
+    def get_date(self):
+        """Генерация даты на завтра/послезавтра"""
+        pass
+
+    def get_timetable(self):
+        """Получение расписания"""
+        fa = FaAPI()
+        mongo_result = self.connector.get_usergroup(self.user_id)
+        group_name, group_id = mongo_result["group_name"], mongo_result["group_id"]
+        #Получаем инфо о расписании группы 
+        timetable = fa.timetable_group(group_id, date, date)
+
+        if len(timetable) == 0:
+            return "Сегодня пар нет, отдыхайте!"
+
+        out_str = "Расписание группы {} на {}:\n".format(group_name, "date")
+        for i in range(len(timetable)):
+            out_str +="{}. {}\n".format(i+1,timetable[i]["discipline"])
+        return out_str
     
