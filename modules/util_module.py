dfb4fd915b9a9041b88bd88a49fb314fe3041fa3
Georgiy Demo <demenchuk.george@protonmail.com>
2020-05-23 12:33:44 +0300

    Детект комманд по токенам

--- modules/util_module.py
+++ modules/util_module.py
@@ -11,6 +11,7 @@ class UtilClass:
             "response": {},
             "version": "1.0",
         }
+
         out_dict['response']['end_session'] = end_session
         out_dict['response']['text'] = text
 
@@ -19,3 +20,21 @@ class UtilClass:
                 {"title": txt, "hide": True} for txt in buttons]
 
         return out_dict
+    
+    @staticmethod
+    def wordintokens_full(words_list, token_list):
+        """Поиск списка слов в токенах, полное совпадение"""
+        return set(words_list) == set(token_list)
+    
+    @staticmethod
+    def wordintokens_any(words_list, token_list):
+        """Поиск списка слов в токенах, совпадение хотя бы по одному слову"""
+        detected_words = []
+        results_list = []
+        for word in words_list:
+            if word in token_list:
+                detected_words.append(word)
+                results_list.append(True)
+            else:
+                results_list.append(False)
+        return any(results_list), detected_words
